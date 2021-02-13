090ff6c844a03010ec6feaaec1c7e5b134fb546d
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-13 12:47:39 -0500

    Se crea proyecto demo-drud-mongo

---
+++ demo-crud-mongo/src/main/java/com/popehiflo/democrudmongo/DemoCrudMongoApplication.java
@@ -0,0 +1,13 @@
+package com.popehiflo.democrudmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudMongoApplication.class, args);
+	}
+
+}
---
+++ demo-crud-mongo/src/test/java/com/popehiflo/democrudmongo/DemoCrudMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popehiflo.democrudmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
