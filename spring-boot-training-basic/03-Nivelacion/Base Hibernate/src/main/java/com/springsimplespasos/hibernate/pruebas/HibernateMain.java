18483967347b1a75c689a414d2218b1141b2f5d4
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-22 11:39:20 -0500

    Hibernate, CRUD simple

--- spring-boot-training-basic/03-Nivelacion/Base Hibernate/src/main/java/com/springsimplespasos/hibernate/pruebas/HibernateMain.java
+++ spring-boot-training-basic/03-Nivelacion/Base Hibernate/src/main/java/com/springsimplespasos/hibernate/pruebas/HibernateMain.java
@@ -1,12 +1,40 @@
 package com.springsimplespasos.hibernate.pruebas;
 
+import com.springsimplespasos.hibernate.entidades.SimpleEntity;
 import com.springsimplespasos.hibernate.util.HibernateUtil;
 import org.hibernate.Session;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 public class HibernateMain {
 
     public static void main(String[] args) {
         Session session = HibernateUtil.getSeccionSessionFactory().openSession();
+        session.beginTransaction();
+
+        /* Create */
+        /*SimpleEntity entity = new SimpleEntity();
+        entity.setNombre("primer row");
+        session.save(entity);*/
+
+        /* Update */
+        /*SimpleEntity entityUpdate = new SimpleEntity();
+        entityUpdate.setCodigo(2);
+        entityUpdate.setNombre("Nombre UPDATE");
+        session.update(entityUpdate);*/
+
+        /* Delete */
+       /* SimpleEntity entityDelete = new SimpleEntity();
+        entityDelete.setCodigo(2);
+        entityDelete.setNombre("Nombre UPDATE");
+        session.delete(entityDelete);*/
+
+        /* Read */
+        SimpleEntity entityRead = session.get(SimpleEntity.class, 1);
+        System.out.println(entityRead.getNombre());
+
+        session.getTransaction().commit();
         session.close();
     }
 
