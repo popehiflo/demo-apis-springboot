d41e2ffcad948f45db49d49f512c4ea736fd7501
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-28 12:27:21 -0500

    SpringBoot, Generando proyecto SpringBoot + SpringDta desde IntelliJ

---
+++ spring-boot-training-basic/04-Iniciando_SpringBoot/Demo Spring Boot/src/main/java/com/popehiflo/demo/demo_universidad/backend/DemoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.popehiflo.demo.demo_universidad.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootApplication.class, args);
+    }
+
+}
---
+++ spring-boot-training-basic/04-Iniciando_SpringBoot/Demo Spring Boot/src/test/java/com/popehiflo/demo/demo_universidad/backend/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popehiflo.demo.demo_universidad.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
